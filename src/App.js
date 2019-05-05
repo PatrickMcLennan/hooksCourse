f2d057216f3877c8d8a0397b7838b466746811cc
Patrick <patrick.a.mclennan@gmail.com>
2019-05-05 12:19:14 -0400

    workin'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const App = () => {
+  ///////////////// GENERAL FORMULA
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useState('');
 
@@ -8,7 +9,11 @@ const App = () => {
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <h3>{name}</h3>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <input
+        type="text"
+        onChange={({ target: { value } }) => setName(value)}
+        value={name}
+      />
     </div>
   );
 };
