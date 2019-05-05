94b31b7584e94dd19a91ab8eb10b662d2947ee75
Patrick <patrick.a.mclennan@gmail.com>
2019-05-05 13:06:59 -0400

    useEffect has blown my mind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ToggleHook from './Toggle/ToggleHook';
 
 const App = () => {
   ///////////////// GENERAL FORMULA
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useState('');
+  useEffect(() => {
+    document.title = name;
+  });
 
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
-      <h3>{name}</h3>
-      <input
-        type="text"
-        onChange={({ target: { value } }) => setName(value)}
-        value={name}
-      />
       <ToggleHook />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}>
+        <input
+          type="text"
+          onChange={({ target: { value } }) => setName(value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
