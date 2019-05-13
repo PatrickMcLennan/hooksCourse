9ce89d7c25836093a44fdc07b3832292e7d4a9c9
Patrick <patrick.a.mclennan@gmail.com>
2019-05-12 20:44:20 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,49 +1,83 @@
-import React, { useRef, createContext, useMemo } from 'react';
+//////////  VIDEO #13
+
+import React, {
+    useRef,
+    createContext,
+    useMemo,
+    useEffect,
+    useState
+} from 'react';
 import ToggleHook from './Toggle/ToggleHook';
 import { useTitleInput } from './hooks/useTitleInput';
-// import Counter from './Counter/Counter';
+import Counter from './Counter/Counter';
 
 export const UserContext = createContext();
 
 const App = () => {
-  ///////////////// GENERAL FORMULA
-  // const [value, setValue] = useState(initialState);
-  const [name, setName] = useTitleInput('');
-  const ref = useRef();
+    ///////////////// GENERAL FORMULA
+    // const [value, setValue] = useState(initialState);
+    const [name, setName] = useTitleInput('');
+    const ref = useRef();
 
-  const reverseWord = word => {
-    console.log('function called');
-    return word
-      .split('')
-      .reverse()
-      .join('');
-  };
+    const [dishes, setDishes] = useState([]);
 
-  const title = 'Level Up Dishes';
-  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+    const reverseWord = word => {
+        console.log('function called');
+        return word
+            .split('')
+            .reverse()
+            .join('');
+    };
 
-  return (
-    <UserContext.Provider value={{ user: false }}>
-      <div className="main-wrapper" ref={ref}>
-        <h1
-          onClick={() =>
-            console.log(ref.current.classList.add('new-fake-class'))
-          }>
-          {TitleReversed}
-        </h1>
-        <ToggleHook />
-        {/* <Counter /> */}
-        <form onSubmit={e => e.preventDefault()}>
-          <input
-            type="text"
-            onChange={({ target: { value } }) => setName(value)}
-            value={name}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    </UserContext.Provider>
-  );
+    const fetchUsers = async () => {
+        const res = await fetch(
+            'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
+        );
+        const dishes = await res.json();
+        setDishes(dishes);
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, [name]);
+
+    const title = 'Level Up Dishes';
+    const TitleReversed = useMemo(() => reverseWord(title), [title]);
+
+    return (
+        <UserContext.Provider value={{ user: false }}>
+            <div className="main-wrapper" ref={ref}>
+                <h1
+                    onClick={() =>
+                        console.log(ref.current.classList.add('new-fake-class'))
+                    }>
+                    {TitleReversed}
+                </h1>
+                <ToggleHook />
+                <Counter />
+                <form onSubmit={e => e.preventDefault()}>
+                    <input
+                        type="text"
+                        onChange={({ target: { value } }) => setName(value)}
+                        value={name}
+                    />
+                    <button>Submit</button>
+                </form>
+
+                {dishes.map(dish => (
+                    <article className="dish-card dish-card--withImage">
+                        <h3>{dish.name}</h3>
+                        <p>{dish.desc}</p>
+                        <div className="ingredients">
+                            {dish.ingredients.map(ingredient => (
+                                <span>{ingredient}</span>
+                            ))}
+                        </div>
+                    </article>
+                ))}
+            </div>
+        </UserContext.Provider>
+    );
 };
 
 export default App;
--- src/Toggle/ToggleHook.js
+++ src/Toggle/ToggleHook.js
@@ -1,17 +1,17 @@
 import React, { useState, useContext } from 'react';
 import { UserContext } from '../App';
+import DishForm from '../DishForm';
 
 const ToggleHook = () => {
-  const [isToggled, setToggle] = useState(false);
-  const userInfo = useContext(UserContext);
-  console.log(`userInfo: ${userInfo}`);
-  if (!userInfo.user) return null;
-  return (
-    <div>
-      <button onClick={() => setToggle(!isToggled)}>hello</button>
-      {isToggled && <h2>hello</h2>}
-    </div>
-  );
+    const [isToggled, setToggle] = useState(false);
+    const userInfo = useContext(UserContext);
+    if (userInfo.user) return null;
+    return (
+        <div>
+            <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+            {isToggled && <DishForm />}
+        </div>
+    );
 };
 
 export default ToggleHook;
