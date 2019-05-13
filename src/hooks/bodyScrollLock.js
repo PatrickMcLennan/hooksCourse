71ac4c0a7e8ec41adb71924eaf28bc534ab1d54d
Patrick <patrick.a.mclennan@gmail.com>
2019-05-12 20:53:45 -0400

    message

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -1,9 +1,14 @@
 import { useLayoutEffect } from 'react';
 
 const useBodyScrollLock = () => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     useLayoutEffect(() => {
         document.body.style.overflow = 'hidden';
-    });
+
+        return () => {
+            document.body.style.overflow = originalOverflow;
+        };
+    }, []);
 };
 
 export default useBodyScrollLock;
